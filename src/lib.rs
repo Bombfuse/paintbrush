cf615deadf8188debf5908721ed5660dd8d820f7
Bombfuse <eiffeldud@gmail.com>
2020-05-08 15:06:21 -0700

    Cornflower blue

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
-// TODO: Cornflower blue
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Color { r, g, b, a }
+    }
+
+    pub fn percentage(&self) -> (f32, f32, f32, f32) {
+        let r = self.r as f32 / 255.0;
+        let g = self.g as f32 / 255.0;
+        let b = self.b as f32 / 255.0;
+        let a = self.a as f32 / 255.0;
+
+        (r, g, b, a)
+    }
+}
+
+pub const CORNFLOWER_BLUE: Color = Color { r: 100, g: 149, b: 237, a: 255 };
